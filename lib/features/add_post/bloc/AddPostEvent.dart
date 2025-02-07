a0200e6c906d12222ff7d9428ca0bb559fb6a318
Mohamed Yasser <mohamedyasser.alkotp@gmail.com>
2025-02-07 21:32:28 +0200

    Delete lib/features/add_post directory

--- lib/features/add_post/bloc/AddPostEvent.dart
+++
@@ -1,8 +0,0 @@
-import 'package:trend/features/add_post/domain/entities/post.dart';
-
-sealed class AddPostEvent1 {}
-
-class AddNewPostEvent extends AddPostEvent1 {
-  final NewPost newPost;
-  AddNewPostEvent(this.newPost);
-}
--- lib/features/add_post/domain/entities/post.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-class NewPost {
-  final File fileImage;
-  final String description;
-
-  NewPost({required this.fileImage, required this.description});
-}
