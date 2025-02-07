1b07aea6ecfc74a2330343eb9cf67659c1e0eb9e
Mohamed Yasser <mohamedyasser.alkotp@gmail.com>
2025-02-07 21:29:24 +0200

    Delete shared directory

--- shared/const/const.dart
+++
@@ -1,7 +0,0 @@
-
-
-const String trendingTags = "TRENDING TAGS";
-const String popularTags = "POPULAR TAGS";
- const String seeAll = "See All";
- const String explorePost = "Explore Post";
- const String accounts = "Accounts";
