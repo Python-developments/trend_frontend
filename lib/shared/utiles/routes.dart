1b0171f9f6075d9621bd765854376102a4e11c72
Mohamed Yasser <mohamedyasser.alkotp@gmail.com>
2025-02-10 14:10:13 +0200

    Add files via upload

--- lib/shared/utiles/routes.dart
+++ lib/shared/utiles/routes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trend/features/auth/presentation/pages/login_screen.dart';
+import 'package:trend/features/auth/presentation/pages/register_screen.dart';
 import 'package:trend/features/authentication/presentation/pages/login.dart';
 import 'package:trend/features/authentication/presentation/pages/reset_password.dart';
 import 'package:trend/features/authentication/presentation/pages/signup.dart';
@@ -7,20 +9,24 @@ import 'package:trend/features/posts/data/models/post_model.dart';
 import 'package:trend/features/posts/presentation/Pages/home_page.dart';
 import 'package:trend/features/profile/presentation/Pages/user_profile/user_profile.dart';
 
+import '../../features/auth/presentation/pages/otp_confirm.dart';
+
 class AppRoutes {
   static const String home = '/home';
   static const String login = '/login';
-  static const String signup = '/signup';
+  static const String register = '/register';
   static const String resetPassword = '/reset-password';
   static const String userProfile = '/UserProfile';
+  static const String otpConfirm = '/otp-confirm';
 }
 
 Map<String, WidgetBuilder> routes = {
-  AppRoutes.login: (context) => LoginPage(),
-  AppRoutes.signup: (context) => SignupPage(),
+  AppRoutes.login: (context) => LoginScreen2(),
+  AppRoutes.register: (context) => RegisterScreen(),
   AppRoutes.resetPassword: (context) => ResetPasswordPage(),
   AppRoutes.home: (context) => MainScreen(),
   AppRoutes.userProfile: (context) => UserProfile(),
+  AppRoutes.otpConfirm: (context) => OtpConfirmScreen(),
 };
 
 Route<dynamic>? onGenerateRoute(RouteSettings settings) {
@@ -28,11 +34,13 @@ Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     case AppRoutes.resetPassword:
       return MaterialPageRoute(builder: (_) => ResetPasswordPage());
     case AppRoutes.login:
-      return MaterialPageRoute(builder: (_) => LoginPage());
-    case AppRoutes.signup:
-      return MaterialPageRoute(builder: (_) => SignupPage());
+      return MaterialPageRoute(builder: (_) => LoginScreen2());
+    case AppRoutes.register:
+      return MaterialPageRoute(builder: (_) => RegisterScreen());
     case AppRoutes.home:
       return MaterialPageRoute(builder: (_) => HomePage());
+    case AppRoutes.otpConfirm:
+      return MaterialPageRoute(builder: (_) => OtpConfirmScreen());
     case AppRoutes.userProfile:
       final model = settings.arguments as PostModel;
       return MaterialPageRoute(builder: (_) => UserProfile());
--- lib/shared/utiles/services_local.dart
+++ lib/shared/utiles/services_local.dart
@@ -1,5 +1,12 @@
 import 'package:get_it/get_it.dart';
 
+import '../../features/auth/data/data_sources/auth_data_source.dart';
+import '../../features/auth/data/repositories/auth_repository.dart';
+import '../../features/auth/domain/repositories/base_auth_repositories.dart';
+import '../../features/auth/domain/use_cases/login_use_case.dart';
+import '../../features/auth/domain/use_cases/register_use_case.dart';
+import '../../features/auth/domain/use_cases/resend_otp_use_case.dart';
+import '../../features/auth/domain/use_cases/verify_otp_use_case.dart';
 import '../../features/explore/data/data_sources/get_post_data_source.dart';
 import '../../features/explore/data/repositories/explore_repository.dart';
 import '../../features/explore/domain/repositories/get_post_base_repository.dart';
@@ -12,12 +19,14 @@ import '../../features/explore/domain/use_cases/user_search_use_case.dart';
 import '../../features/explore/presentation/manager/explore/explore_bloc.dart';
 import '../../features/explore/presentation/manager/explore/explore_event.dart';
 
+
 final sl = GetIt.instance;
 
 class ServiceLocator {
   void init() {
     // this is for explore block injection
-    sl.registerFactory(() => ExploreBloc(getPostsUseCase: sl(),getPostWithPageNumberUseCase:  sl())
+    sl.registerFactory(() => ExploreBloc(
+        getPostsUseCase: sl(), getPostWithPageNumberUseCase: sl())
       ..add(GetAllPostsEvent()));
 
     // this is for data source injection
@@ -39,9 +48,35 @@ class ServiceLocator {
         () => HashtagsUseCase(baseGetPostRepository: sl()));
     sl.registerLazySingleton(() =>
         GetPostWithPageNumberUseCase(baseGetPostRepository: sl()));
-    sl.registerLazySingleton(() =>
-        UserSearchUseCase(baseGetPostRepository: sl()));
-    sl.registerLazySingleton(() =>
-        LocationSearchUseCase(baseGetPostRepository: sl()));
+    sl.registerLazySingleton(
+        () => UserSearchUseCase(baseGetPostRepository: sl()));
+    sl.registerLazySingleton(
+        () => LocationSearchUseCase(baseGetPostRepository: sl()));
+
+    //////////////////////////////////////////////////////////////////
+
+    // this is for auth
+    
+    // sl.registerFactory(() => AuthBloc(loginUseCase: sl(), registerUseCase: sl()));
+
+    
+    // this is for data source injection
+    sl.registerLazySingleton<BaseAuthDataSource>(
+        () => AuthDataSourceImpl());
+
+    // this is for repository injection
+    sl.registerLazySingleton<BaseAuthRepository>(() => AuthRepository(
+          baseAuthDataSource: sl(),
+        ));
+
+    // this is for use case injection
+    sl.registerLazySingleton(
+        () => LoginUseCase(authRepository: sl()));
+    sl.registerLazySingleton(
+        () => RegisterUseCase(authRepository: sl()));
+    sl.registerLazySingleton(
+        () => ResendOtpUseCase(authRepository: sl()));
+    sl.registerLazySingleton(
+        () => VerifyOtpUseCase(authRepository: sl()));
   }
 }
